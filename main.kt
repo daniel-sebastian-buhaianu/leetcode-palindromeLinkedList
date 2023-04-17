9cf5c3d419c1e7870c4c84785f258ed231d4ad22
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-17 13:36:42 +0300

    last solution didn't wrong, so I improved even further, now it works

--- main.kt
+++ main.kt
@@ -1,7 +1,7 @@
 fun main() {
     // head = [1, 2, 2, 1]
     val n1 = ListNode(1)
-    val n2 = ListNode(2)
+    val n2 = ListNode(1)
     val n3 = ListNode(2)
     val n4 = ListNode(1)
 
@@ -21,23 +21,43 @@ class ListNode(var `val`: Int) {
 }
 
 fun isPalindrome(head: ListNode?): Boolean {
-    var ptr: ListNode? = head
-    var number: String = ""
-    while (ptr != null) {
-        number += ptr.`val`
-        ptr = ptr.next
+    // Find the middle element
+    var slowPtr = head
+    var fastPtr = head
+    while (fastPtr != null && fastPtr.next != null) {
+        slowPtr = slowPtr?.next
+        fastPtr = fastPtr.next
+        fastPtr = fastPtr?.next
     }
 
-    val mid: Int = number.length / 2
-    var i = 0
-    var j = number.length - 1
-    var end = if (number.length % 2 == 0) mid else mid+1
-    while (i < mid && j >= end) {
-        if (number[i] != number[j])
-            return false
-        i++
-        j--
+    // If number of nodes is odd, then move slowptr to the right
+    if (fastPtr != null && fastPtr.next == null) {
+        slowPtr = slowPtr?.next
     }
+
+    // Reverse the second half
+    var prev: ListNode? = null
+    var temp: ListNode?
+    while (slowPtr != null && slowPtr.next != null) {
+        temp = slowPtr.next
+        slowPtr.next = prev
+        prev = slowPtr
+        slowPtr = temp
+    }
+    if (slowPtr != null) {
+        slowPtr.next = prev
+    }
+
+    // Compare first half with second half
+    // If we find any inequality, return false
+    fastPtr = head
+    while (slowPtr != null && fastPtr != null) {
+        if (slowPtr.`val` != fastPtr.`val`) {
+            return false
+        }
+        slowPtr = slowPtr.next
+        fastPtr = fastPtr.next
+    } 
 
     return true
 }
